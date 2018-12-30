e76b24e337e5e0928a7ea515b0dc8f3c2ada5479
Hideyuki Takei <takehide22@gmail.com>
2018-12-30 20:19:16 +0900

    Add new parts cad data

--- arduino/WaterPump/WaterPump.ino
+++ arduino/WaterPump/WaterPump.ino
@@ -7,6 +7,13 @@ const int ledPin =  13;
 
 VL53L0X sensor;
 
+const int maxWaterRange = 5;
+bool isRunning = false;
+
+const int rangeMax = 10;
+int ranges[10];
+int rangeIndex = 0;
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
@@ -15,25 +22,43 @@ void setup() {
   pinMode(motorPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
 
+  digitalWrite(motorPin, LOW);
+
   sensor.init();
   sensor.setTimeout(500);
   sensor.startContinuous();
 }
 
+int getRange() {
+   int sum = 0;
+   const int total = 20;
+   for (int i = 0; i < total; i++) {
+     sum = sum + sensor.readRangeContinuousMillimeters();
+     delay(10);
+   }
+   return sum / total;
+}
+
 void loop() {
+  int range = getRange();
+  Serial.println(range);
+
   if(!digitalRead(buttonPin)) {
     // push button
     digitalWrite(ledPin, HIGH);
-    digitalWrite(motorPin, HIGH);
-    //analogWrite(motorPin, 200);   
+    //digitalWrite(motorPin, HIGH);
+
+    if (!isRunning && range > maxWaterRange) {
+      isRunning = true;
+      digitalWrite(motorPin, HIGH);
+    }
   } else {
     digitalWrite(ledPin, LOW);
-    digitalWrite(motorPin, LOW);
-    //analogWrite(motorPin, 0);
+    //digitalWrite(motorPin, LOW);
   }
 
-  Serial.print(sensor.readRangeContinuousMillimeters());
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-
-  Serial.println();
+  if (isRunning && range <= maxWaterRange) {
+     isRunning = false;
+     digitalWrite(motorPin, LOW);
+  }
 }
